26442b20fe827b2e93346dd270f8e40b2d60b37c
mramos <marcalramos@gmail.com>
2015-09-13 20:46:17 +0000

    create parcials

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   resources :articles do
     resources :comments
   end
-get 'home', to: 'pages#home'
-root 'welcome#index'
+root 'pages#home'
+#root 'welcome#index'
 
  
 end
